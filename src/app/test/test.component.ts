833b7c9ed5f1da6ec2f0f50759330daf68481974
Daniel Paul <daniel.paul@codgency.fr>
2021-04-15 09:29:38 +0200

    CRUD

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,16 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 
+  idElementModification: number;
   nouvelElement: string;
 
   tableau = [
     {
       id: 1,
-      label: 'Test label 1',
+      label: 'Test label 1'
     },
     {
       id: 2,
-      label: 'Test label 2',
+      label: 'Test label 2'
     }
   ];
 
@@ -26,12 +27,23 @@ export class TestComponent implements OnInit {
   }
 
   valider() {
-    this.tableau.push({
-      id: this.tableau.length + 1,
-      label: this.nouvelElement,
-    });
+    if(this.idElementModification && this.idElementModification > 0){
+      let index = this.tableau.findIndex(x=>x.id === this.idElementModification);
+      this.tableau[index].label = this.nouvelElement;
+    }else{
+      this.tableau.push({
+        id: this.tableau.length + 1,
+        label: this.nouvelElement,
+      });
+    }
 
+    this.idElementModification = null;
     this.nouvelElement = '';
+  }
+
+  modifier(element: any){
+    this.idElementModification = element.id;
+    this.nouvelElement = element.label;
   }
 
   supprimer(id: number) {
